7f47764ab4aca5d29e93a8300c22380035b81603
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2024-04-06 16:20:31 +0530

    replaced dmenu with bemenu

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "RobotoMono Nerd Font:size=12" };
-static const char dmenufont[]       = "RobotoMono Nerd Font:size=12";
+static const char dmenufont[]       = "RobotoMono Nerd Font 14";
 static const char col_gray1[]       = "#282c34";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "bemenu-run", "-l", "10", "--fn", dmenufont, "-c", "--nb", col_cyan, "--hb",  col_gray1, "--ab", col_cyan, "--nf", col_gray4, "--hf", col_gray4, "--fb", col_cyan, "-p", "", "-W", "0.3", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *webcmd[]  = { "midori", NULL };
 static const char *explorercmd[]  = { "caja", NULL };
