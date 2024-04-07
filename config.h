08b16b471be49b8cffcefe1b644a840055b4912b
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2024-04-07 20:11:30 +0530

    added numix palette

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -12,15 +12,15 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "RobotoMono Nerd Font:size=13" };
 static const char dmenufont[]       = "RobotoMono Nerd Font 14";
-static const char col_gray1[]       = "#333333";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#212121";
+static const char col_bg1[]       = "#2D2D2D";
+static const char col_fg2[]       = "#3D3D3D";
+static const char col_fg1[]       = "#DCDCDC";
+static const char col_bg2[]       = "#D64937";
+static const char col_border[]    = "#535353";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_cyan, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray1,  col_cyan  },
+	[SchemeNorm] = { col_fg1, col_bg1,   col_fg1},
+	[SchemeSel]  = { col_bg1, col_bg2,  col_fg1},
 };
 
 /* tagging */
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "bemenu-run", "-l", "10", "--fn", dmenufont, "-c", "--nb", col_cyan, "--hb",  col_gray1, "--ab", col_cyan, "--nf", col_gray3, "--hf", col_gray4, "--fb", col_cyan, "-p", "", "-W", "0.3", NULL };
+static const char *dmenucmd[] = { "bemenu-run", "-l", "10", "--fn", dmenufont, "-c", "--nb", col_bg1, "--hb",  col_bg2, "--ab", col_bg1, "--nf", col_fg1, "--hf", col_bg1, "--fb", col_fg2, "--ff", col_fg1, "--cb", col_fg1, "-p", "", "-W", "0.3", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *webcmd[]  = { "midori", NULL };
 static const char *explorercmd[]  = { "caja", NULL };
